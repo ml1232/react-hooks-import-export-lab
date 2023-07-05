180fc345228246394197e736f3c461eb3f7b829e
Matthew <matthewjaredlevin88@gmail.com>
2023-07-05 16:20:28 -0500

    Complete

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
-const username = "Liza";
-const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+export const username = "Liza"
 
-// export all three variables as named exports
+export const city = "New York"
+
+export const image = "https://i.imgur.com/mV8PQxj.gif"
+
